fd51ab94c82cb68d56ffd3463e16cfffc8966815
Jolanta B <yolanta.b@gmail.com>
2018-06-25 19:03:48 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+// import the Google Maps API Wrapper from google-maps-react
+import {GoogleApiWrapper } from 'google-maps-react';
+// import child component
+import MapContainer from './MapContainer';
+class App extends Component {
+  
+
+
+  render() {
+    return (
+      
+      
+      <div id ="map">
+       
+         <MapContainer google={this.props.google}>
+        </MapContainer>
+      </div>
+      
+    );
+  }
+}
+// OTHER MOST IMPORTANT: Here we are exporting the App component WITH the GoogleApiWrapper. You pass it down with an object containing your API key
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAIKAWJPVWuslzcSYcy59HpemVa0hmuiQM',
+})(App)
